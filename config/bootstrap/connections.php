913c48b2914a7686bcaccb80b574acc42d5dcc21
gwoo <gwoohoo@gmail.com>
2011-06-22 20:10:59 -0700

    adding connection

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -40,11 +40,11 @@ use lithium\data\Connections;
 /**
  * Uncomment this configuration to use MongoDB as your default database.
  */
-// Connections::add('default', array(
-// 	'type' => 'MongoDb',
-// 	'host' => 'localhost',
-// 	'database' => 'my_app'
-// ));
+Connections::add('default', array(
+	'type' => 'MongoDb',
+	'host' => 'localhost',
+	'database' => 'php_needs'
+));
 
 /**
  * Uncomment this configuration to use CouchDB as your default database.
